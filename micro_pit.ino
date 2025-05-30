5b1292fba1c4466874be9bfbcbbc6921cbe0dae8
Bobert <salvo.robertroy@gmail.com>
2025-05-30 23:43:32 +0800

    chore: removed lcd code

--- micro_pit.ino
+++ micro_pit.ino
@@ -1,10 +1,10 @@
 #include <Wire.h>
-#include <LiquidCrystal_I2C.h>
 #include <WiFi.h>
 #include <HTTPClient.h>
+#include <WebServer.h>
 
-const char* ssid = "SKYWORTH-7197";
-const char* password = "352191111";
+const char* ssid = "Alberttt";
+const char* password = "iloveyou3000";
 
 const char* serverUrl = "http://192.168.0.42:3000/api/alert";
 
@@ -17,7 +17,21 @@ int scl = 22;
 
 int soundLevel;
 
-LiquidCrystal_I2C lcd(0x27, 16, 2); 
+WebServer server(80);
+
+void manualAlertOn() {
+  Serial.println("turning on");
+  digitalWrite(buzzer, HIGH);
+  digitalWrite(led, HIGH);
+  server.send(200, "text/plain", "Alert ON");
+}
+
+void manualAlertOff() {
+  Serial.println("turning off");
+  digitalWrite(buzzer, LOW);
+  digitalWrite(led, LOW);
+  server.send(200, "text/plain", "Alert OFF");
+}
 
 void setup() {
   Serial.begin(115200);  
@@ -33,48 +47,48 @@ void setup() {
     Serial.print(".");
   }
   Serial.println("\nConnected to WiFi");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
 
-  // Initialize LCD
-  lcd.begin(16, 2);
-  lcd.backlight();
-  lcd.setCursor(0, 0);
-  lcd.print("Monitoring...");
+  server.on("/alert-on", HTTP_GET, manualAlertOn);
+  server.on("/alert-off", HTTP_GET, manualAlertOff);
+
+  server.begin();
+  Serial.println("HTTP server started");
 }
 
 void loop() {
+  server.handleClient();
+
   soundLevel = analogRead(soundSensorPin);
   Serial.println(soundLevel);  
 
   if (soundLevel >= 4095) {
     digitalWrite(buzzer, HIGH);
     digitalWrite(led, HIGH);
-    lcd.setCursor(0, 0);
-    lcd.print("Warning:        "); 
-    lcd.setCursor(0, 1);
-    lcd.print("Illegal Activity");
 
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http;
-    http.begin(serverUrl);
-    http.addHeader("Content-Type", "application/json");
+    if (WiFi.status() == WL_CONNECTED) {
+      HTTPClient http;
+      http.begin(serverUrl);
+      http.addHeader("Content-Type", "application/json");
 
-    String jsonPayload = "{\"activity\": \"Alert Triggered\", \"soundLevel\": " + String(soundLevel) + ", \"timestamp\": " + String(millis()) + "}";
+      String jsonPayload = "{\"activity\": \"Alert Triggered\", \"soundLevel\": " + String(soundLevel) + ", \"timestamp\": " + String(millis()) + "}";
 
-    int httpResponseCode = http.POST(jsonPayload);
+      int httpResponseCode = http.POST(jsonPayload);
 
-    Serial.print("HTTP Response code: ");
-    Serial.println(httpResponseCode);
+      Serial.print("HTTP Response code: ");
+      Serial.println(httpResponseCode);
 
-    if (httpResponseCode > 0) {
-      String response = http.getString();
-      Serial.println("Server response:");
-      Serial.println(response);
-    } else {
-      Serial.println("Error in HTTP request");
+      if (httpResponseCode > 0) {
+        String response = http.getString();
+        Serial.println("Server response:");
+        Serial.println(response);
+      } else {
+        Serial.println("Error in HTTP request");
+      }
+
+      http.end();
     }
-
-    http.end();
-  }
     delay(500); 
     digitalWrite(led, LOW);
     digitalWrite(buzzer, LOW);
@@ -82,13 +96,7 @@ void loop() {
   } else {
     digitalWrite(buzzer, LOW);
     digitalWrite(led, LOW);
-    lcd.setCursor(0, 0);
-    lcd.print("Monitoring...   "); 
-    lcd.setCursor(0, 1);
-    lcd.print("                "); 
   }
 
   delay(100);  
 }
-
-
