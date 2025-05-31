00305efb1d2eba2d7ce9dffed5c12d2a3c5a742d
Bobert <salvo.robertroy@gmail.com>
2025-06-01 03:31:50 +0800

    feat: polished manual alert

--- micro_pit.ino
+++ micro_pit.ino
@@ -3,39 +3,39 @@
 #include <HTTPClient.h>
 #include <WebServer.h>
 
-const char* ssid = "Alberttt";
-const char* password = "iloveyou3000";
+const char* ssid = "SKYWORTH-7197";
+const char* password = "352191111";
 
-const char* serverUrl = "http://192.168.0.38:3000/api/alert";
+const char* serverUrl = "http://192.168.0.42:3000/api/alert";
  
 int buzzer = 23;
-int led = 12; 
-const int soundSensorPin = 35;  
-int sda = 21;
-int scl = 22;
+int led = 26; 
+const int soundSensorPin = 34;  
+bool manualMode = false;
 
 int soundLevel;
 
 WebServer server(80);
 
 void manualAlertOn() {
-  Serial.println("turning on");
+  Serial.println("turning on manually");
   digitalWrite(buzzer, HIGH);
   digitalWrite(led, HIGH);
+  manualMode = true;
+
   HTTPClient http;
   http.begin(serverUrl);
   http.addHeader("Content-Type", "application/json");
 
-  String jsonPayload = "{\"activity\": \"Alert Triggered\", \"soundLevel\": " + String(soundLevel) + "}";
+  String jsonPayload = "{\"activity\": \"Manual Alert\", \"soundLevel\": " + String(soundLevel) + "}";
 
   int httpResponseCode = http.POST(jsonPayload);
-
   Serial.print("HTTP Response code: ");
   Serial.println(httpResponseCode);
 
   if (httpResponseCode > 0) {
     String response = http.getString();
-    Serial.println("Server response:"); 
+    Serial.println("Server response:");
     Serial.println(response);
   } else {
     Serial.println("Error in HTTP request");
@@ -46,31 +46,14 @@ void manualAlertOn() {
 }
 
 void manualAlertOff() {
-  Serial.println("turning off");
+  Serial.println("turning off manually");
   digitalWrite(buzzer, LOW);
   digitalWrite(led, LOW);
-  HTTPClient http;
-  http.begin(serverUrl);
-  http.addHeader("Content-Type", "application/json");
+  manualMode = false;
 
-  String jsonPayload = "{\"activity\": \"Alert Triggered\", \"soundLevel\": " + String(soundLevel) + "}";
-
-  int httpResponseCode = http.POST(jsonPayload);
-
-  Serial.print("HTTP Response code: ");
-  Serial.println(httpResponseCode);
-
-  if (httpResponseCode > 0) {
-    String response = http.getString();
-    Serial.println("Server response:"); 
-    Serial.println(response);
-  } else {
-    Serial.println("Error in HTTP request");
-  }
-
-  http.end();
   server.send(200, "text/plain", "Alert OFF");
 }
+
 
 void setup() {
   Serial.begin(115200);  
@@ -99,44 +82,46 @@ void setup() {
 void loop() {
   server.handleClient();
 
-  soundLevel = analogRead(soundSensorPin);
-  Serial.println(soundLevel);  
+  if (!manualMode) {
+    soundLevel = analogRead(soundSensorPin);
+    Serial.println(soundLevel);
 
-  if (soundLevel >= 3000) {
-    // digitalWrite(buzzer, HIGH);
-    Serial.println("buzzer sound");
-    digitalWrite(led, HIGH);
+    if (soundLevel >= 3000) {
+      Serial.println("buzzer sound");
+      digitalWrite(buzzer, HIGH);
+      digitalWrite(led, HIGH);
 
-    if (WiFi.status() == WL_CONNECTED) {
-      HTTPClient http;
-      http.begin(serverUrl);
-      http.addHeader("Content-Type", "application/json");
+      if (WiFi.status() == WL_CONNECTED) {
+        HTTPClient http;
+        http.begin(serverUrl);
+        http.addHeader("Content-Type", "application/json");
 
-      String jsonPayload = "{\"activity\": \"Alert Triggered\", \"soundLevel\": " + String(soundLevel) + "}";
+        String jsonPayload = "{\"activity\": \"Auto Alert\", \"soundLevel\": " + String(soundLevel) + "}";
 
-      int httpResponseCode = http.POST(jsonPayload);
+        int httpResponseCode = http.POST(jsonPayload);
 
-      Serial.print("HTTP Response code: ");
-      Serial.println(httpResponseCode);
+        Serial.print("HTTP Response code: ");
+        Serial.println(httpResponseCode);
 
-      if (httpResponseCode > 0) {
-        String response = http.getString();
-        Serial.println("Server response:"); 
-        Serial.println(response);
-      } else {
-        Serial.println("Error in HTTP request");
+        if (httpResponseCode > 0) {
+          String response = http.getString();
+          Serial.println("Server response:");
+          Serial.println(response);
+        } else {
+          Serial.println("Error in HTTP request");
+        }
+
+        http.end();
       }
-
-      http.end();
+      
+      digitalWrite(buzzer, LOW);
+      digitalWrite(led, LOW);
+      delay(500);
+    } else {
+      digitalWrite(buzzer, LOW);
+      digitalWrite(led, LOW);
     }
-    delay(500); 
-    digitalWrite(led, LOW);
-    digitalWrite(buzzer, LOW);
-    delay(500); 
-  } else {
-    digitalWrite(buzzer, LOW);
-    digitalWrite(led, LOW);
   }
 
-  delay(100);  
+  delay(100);
 }
